a96c92dccfd34abbf7a76797868e2eaf6072cbfb
681821 <681821@PC257180.cts.com>
2018-03-14 19:20:26 +0530

    First http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,7 +26,7 @@ import { FormsModule } from "@angular/forms"
     CustomerComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, FormsModule
+    BrowserModule, AppRoutingModule, FormsModule,HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/item-menu-service.ts
+++ src/app/services/item-menu-service.ts
@@ -1,17 +1,33 @@
 import { ItemMenu } from '../model/item-menu-model';
+import { Http,Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
 
-
+@Injectable()
 export class ItemMenuService{
     private crackersArray:ItemMenu[]=[];
-    constructor(){
-        let icon1=new ItemMenu('/assets/menu-icons/001-rocket-1.svg','Rocket','rockets');
+
+    constructor(private http:Http){
+
+        this.getIcons().subscribe(
+            (response)=>{console.log(response);
+             let JSONdata=response.json();
+             JSONdata.items.array.forEach(element => {
+                let icon=new ItemMenu(element.itemIMGPath,element.itemName,element.itemURL);
+                this.crackersArray.push(icon);
+             });
+            },
+            (error)=>{console.log(error)});
+
+/*         let icon1=new ItemMenu('/assets/menu-icons/001-rocket-1.svg','Rocket','rockets');
         let icon2=new ItemMenu('/assets/menu-icons/002-rocket.svg','Bomb','bombs');
         let icon3=new ItemMenu('/assets/menu-icons/003-magic.svg','FireStick','firesticks');
         let icon4=new ItemMenu('/assets/menu-icons/004-rocket-2.svg','Pot','pots');
             this.crackersArray.push(icon1);
             this.crackersArray.push(icon2);
             this.crackersArray.push(icon3);
-            this.crackersArray.push(icon4);   
+            this.crackersArray.push(icon4);  */
+            
+          
     }
     public getCrarckerIconsArray():ItemMenu[]{
         return this.crackersArray;
@@ -23,6 +39,10 @@ export class ItemMenuService{
 
     public setCrarckerIconsArrayItem(cracker:ItemMenu){
        this.crackersArray.push(cracker);
+    }
+
+    public getIcons(){
+        return this.http.get('https://localhost:8080/kundalini/rs/itemCategories/');
     }
 }
 
