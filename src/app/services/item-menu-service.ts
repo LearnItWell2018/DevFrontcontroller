2feaaa34ba8c810570386ee04e509fae9f62e870
681821 <681821@PC257180.cts.com>
2018-03-15 19:46:26 +0530

    grid start

--- src/app/services/item-menu-service.ts
+++ src/app/services/item-menu-service.ts
@@ -17,17 +17,7 @@ export class ItemMenuService{
              });
             },
             (error)=>{console.log(error)});
-
-/*         let icon1=new ItemMenu('/assets/menu-icons/001-rocket-1.svg','Rocket','rockets');
-        let icon2=new ItemMenu('/assets/menu-icons/002-rocket.svg','Bomb','bombs');
-        let icon3=new ItemMenu('/assets/menu-icons/003-magic.svg','FireStick','firesticks');
-        let icon4=new ItemMenu('/assets/menu-icons/004-rocket-2.svg','Pot','pots');
-            this.crackersArray.push(icon1);
-            this.crackersArray.push(icon2);
-            this.crackersArray.push(icon3);
-            this.crackersArray.push(icon4);  */
-            
-          
+     
     }
     public getCrarckerIconsArray():ItemMenu[]{
         return this.crackersArray;
