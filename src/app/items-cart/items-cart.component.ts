3dad20c7b6f1adeeb1e9994b209a7f7e0ebc9b01
681821 <681821@PC257180.cts.com>
2018-03-22 18:24:17 +0530

    Finalizing Cart

--- src/app/items-cart/items-cart.component.ts
+++ src/app/items-cart/items-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GridItem } from '../model/item-grid-models';
 
 @Component({
   selector: 'app-items-cart',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items-cart.component.css']
 })
 export class ItemsCartComponent implements OnInit {
-
+  private myKartList:GridItem[];
+  private totalValue:number=0;
   constructor() { }
 
   ngOnInit() {
+    let myKart=localStorage.getItem('myKart');
+    this.myKartList=JSON.parse('['+myKart+']');
+    this.myKartList.forEach(function(item){
+      item["totalFinalVal"]=item.itemQuantity*item.itemPrice;
+    });
+    this.syncWithKart();
+    this.calculateTotal();
+    //let myKartModify=localStorage.setItem('myKartModify');
   }
 
+  reduceQuantity(index){
+    if(this.myKartList[index].itemQuantity===0){
+      return false;
+    }else{
+     this.myKartList[index].itemQuantity=this.myKartList[index].itemQuantity-1;
+     this.myKartList[index]["totalFinalVal"]=this.myKartList[index].itemQuantity*this.myKartList[index].itemPrice;
+     this.syncWithKart();
+     this.calculateTotal();
+    }
+    
+ }
+ 
+ increaseQuantity(index){
+   if(this.myKartList[index].itemQuantity===1000){
+     return false;
+   }else{
+    this.myKartList[index].itemQuantity=this.myKartList[index].itemQuantity+1;
+    this.myKartList[index]["totalFinalVal"]=this.myKartList[index].itemQuantity*this.myKartList[index].itemPrice;
+    this.syncWithKart();
+    this.calculateTotal();
+  }
+
+ }
+
+ removeThisProduct(i){
+   this.myKartList.splice(i,1);
+   this.syncWithKart();
+   this.calculateTotal();
+ }
+
+ syncWithKart(){
+  let stripObj=JSON.stringify(this.myKartList).split('[')[1].split(']')[0];
+  localStorage.setItem('myKart',stripObj); 
+ }
+
+calculateTotal(){
+  this.myKartList.forEach(function(item){
+    this.totalValue=this.totalValue+item.totalFinalVal;
+  })
+ }
 }
--- src/app/model/item-grid-models.ts
+++ src/app/model/item-grid-models.ts
@@ -9,6 +9,7 @@ export class GridItem{
                         itemDetails:string;
 						itemAdded:boolean;
 						itemQuantity: number;
+						totalFinalVal:number;
 
                         constructor(  productId : string,productImgPath : string,brand : string,itemName: string,
 						itemPrice: number,itemStock: string,itemActive: boolean,itemDetails:string){
@@ -21,6 +22,7 @@ export class GridItem{
 						this.itemActive=itemActive;
 						this.itemDetails=itemDetails;
 						this.itemAdded=false;
-                        this.itemQuantity=0;
+						this.itemQuantity=0;
+						this.totalFinalVal=0;
                         }
                     }                        
