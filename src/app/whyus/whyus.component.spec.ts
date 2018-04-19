f5571f8f4a1823106b802173afdeb3ba4d59ae12
272033 <272033@PC300726.cts.com>
2018-04-19 16:09:16 +0530

    Fixing Whyus

--- src/app/whyus/whyus.component.spec.ts
+++ src/app/whyus/whyus.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WhyusComponent } from './whyus.component';
 
+
 describe('WhyusComponent', () => {
   let component: WhyusComponent;
   let fixture: ComponentFixture<WhyusComponent>;
--- src/app/whyus/whyus.component.ts
+++ src/app/whyus/whyus.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 @Component({
   selector: 'app-whyus',
   templateUrl: './whyus.component.html',
   styleUrls: ['./whyus.component.css']
 })
-export class WhyusComponent implements OnInit {
-
+export class WhyusComponent {
+  @ViewChild("myObject") myObject:any;
+  public allQuotes: string[] = ['700 minimum order', 'fast delivery', 'quality of delivery'];
+  public elems: number = 0;
+  public intervals: any;
+  public quotesVal: string;
   constructor() { }
 
-  ngOnInit() {
-  }
+  public rollOnMouseOver(event) {
+    this.intervals = setInterval(() => {
+      this.quotesVal = this.allQuotes[this.elems]
+      if (this.elems === 2) { this.elems = 0; } else { this.elems++; }
+      console.log(this.myObject.classList);
+      //this.myObject..classList.add("animated", "infinite", "bounce");
+    }, 1999);
+  };
+
+
+
+
+  public stopRoll(event) {
+    clearInterval(this.intervals);
+    //this.myObject.classList.remove("animated", "infinite", "bounce");
+  };
+
 
 }
