e0a509c1135d619fcb7d74d5798a160002e8bac4
unknown <LearnItWell2018@gmail.com>
2018-05-09 00:59:28 +0530

    Get Item

--- src/app/header/typeahead-template/typeahead-template.component.ts
+++ src/app/header/typeahead-template/typeahead-template.component.ts
@@ -1,69 +1,78 @@
-import {Component} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
 import { OnInit } from '@angular/core';
-import { Http,Headers } from '@angular/http';
+import { Http, Headers } from '@angular/http';
+import { GridItem } from '../../model/item-grid-models';
 
-let itemsWithImage;
+let itemsInList;
+let itemSelected;
 
 @Component({
   selector: 'ngbd-typeahead-template',
   templateUrl: './typeahead-template.component.html',
   styleUrls: ['./typeahead-template.component.css']
 })
-export class TypeaheadTemplateComponent  implements OnInit {
+export class TypeaheadTemplateComponent implements OnInit {
   public model: any;
-  
-  constructor(private http:Http){ }
-  ngOnInit(){
+  public gridItem: GridItem;
+
+  constructor(private http: Http) { }
+  ngOnInit() {
     this.http.get('https://kundalini.cfapps.io/rs/items/list').subscribe(
-       (response)=>{
-        itemsWithImage=response.json();
-        console.log('itemsWithImage:'+itemsWithImage);
+      (response) => {
+        itemsInList = response.json();
+        console.log('itemsWithImage:' + itemsInList);
       },
-       (error)=>{console.log(error)})
+      (error) => { console.log(error) })
   }
 
   search = (text$: Observable<string>) =>
     text$
       .debounceTime(200)
       .map(term => term === '' ? []
-        : itemsWithImage.filter(v => v.itemName.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
+        : itemsInList.filter(v => v.itemName.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
 
-  formatter = (x: {itemName: string}) => x.itemName;
+  formatter = (x: { itemName: string }) => x.itemName;
 
-  addItemOnClicked(object){
-    console.log(object.target);
+  addItemOnClicked(object) {
+    console.log(object.target.id);
+    console.log(this.findItemDetails(object.target.id));
   }
 
-  addProductToWebStore(index:number,inputElem:HTMLFormElement){
-    if(itemsWithImage[index].itemQuantity===0){
-      alert('Error');
-    }else{
-      if(!localStorage.getItem('myKart')){
-        localStorage.setItem('myKart',JSON.stringify(itemsWithImage[index]));
-        itemsWithImage[index].itemQuantity=0;
-      }else{
-        let myKart=localStorage.getItem('myKart');
-        let presentItem=itemsWithImage[index];
-        if(myKart.indexOf(itemsWithImage.productId)>=0){
-          let myObject=JSON.parse('['+myKart+']');
-          myObject.forEach(function(item){
-            if(item.productId===presentItem.productId){
-              item.itemQuantity=item.itemQuantity+presentItem.itemQuantity;
-              let stripObj=JSON.stringify(myObject).split('[')[1].split(']')[0];
-              localStorage.setItem('myKart',stripObj);
-              presentItem.itemQuantity=0;
-              return;
-            }
-          });
-        }
-        else{
-        myKart=myKart+","+JSON.stringify(itemsWithImage[index]);
-        localStorage.setItem('myKart',myKart);
-        itemsWithImage[index].itemQuantity=0;
-        }
+  findItemDetails(itemID: String) : GridItem {
+    this.http.get('https://kundalini.cfapps.io/rs/items/' + itemID.split("-")[0] + '/' + itemID).subscribe(
+      (response) => {
+        itemSelected = response.json();
+        console.log(itemSelected.productImgPath); 
+        return new GridItem(itemSelected.productId,itemSelected.productImgPath,itemSelected.brand,itemSelected.itemName, itemSelected.itemDesc,
+          itemSelected.itemPrice,itemSelected.itemStock,itemSelected.itemActive,itemSelected.itemDetails);
+      },
+      (error) => { console.log(error) })
+    return this.gridItem;
+  }
+
+  addProductToWebStore(presentItem: GridItem) {
+    console.log(presentItem);
+    if (!localStorage.getItem('myKart')) {
+      localStorage.setItem('myKart', JSON.stringify(presentItem));
+    } else {
+      let myKart = localStorage.getItem('myKart');
+      if (myKart.indexOf(itemsInList.productId) >= 0) {
+        let myObject = JSON.parse('[' + myKart + ']');
+        myObject.forEach(function (item) {
+          if (item.productId === presentItem.productId) {
+            item.itemQuantity = item.itemQuantity + presentItem.itemQuantity;
+            let stripObj = JSON.stringify(myObject).split('[')[1].split(']')[0];
+            localStorage.setItem('myKart', stripObj);
+            return;
+          }
+        });
+      }
+      else {
+        myKart = myKart + "," + JSON.stringify(presentItem);
+        localStorage.setItem('myKart', myKart);
       }
     }
   }
