2f8ad5deb9c376f4ca694665c49b3ac47db24183
272033 <272033@PC300726.cts.com>
2018-05-18 13:35:20 +0530

    Menu Color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './services/auth-service';
 export class AppComponent {
   title = 'app';
 
- public edited = false;
+ public edited = true;
 
   constructor(public auth: AuthService) {
     auth.handleAuthentication();
