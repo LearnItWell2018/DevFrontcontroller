14e812dd1f839909ffe769cade74f479e92adfe0
unknown <LearnItWell2018@gmail.com>
2018-09-09 09:39:33 +0530

    fixing product order

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GridItem } from '../model/item-grid-models';
+import { UtilityService } from '../services/utility-service';
 
 @Component({
   selector: 'app-product',
@@ -11,7 +12,7 @@ export class ProductComponent implements OnInit {
   public selectedProduct:GridItem;
   public items: GridItem[] = [];
 
-  constructor() {
+  constructor(private utility: UtilityService) {
     this.items.push(JSON.parse(localStorage.getItem("selectedProduct")));
     this.items.push(JSON.parse(localStorage.getItem("selectedProduct")));
     this.items.push(JSON.parse(localStorage.getItem("selectedProduct")));
@@ -44,5 +45,37 @@ export class ProductComponent implements OnInit {
     } else {
       return false;
     }
+  }
+
+  addProductToWebStore() {
+    if (this.selectedProduct.itemQuantity === 0) {
+      alert('Error');
+    } else {
+      if (!localStorage.getItem('myKart')) {
+        localStorage.setItem('myKart', JSON.stringify(this.selectedProduct));
+        this.selectedProduct.itemQuantity = 0;
+      } else {
+        let myKart = localStorage.getItem('myKart');
+        let presentItem = this.selectedProduct;
+        if (myKart.indexOf(this.selectedProduct.productId) >= 0) {
+          let myObject = JSON.parse('[' + myKart + ']');
+          myObject.forEach(function (item) {
+            if (item.productId === presentItem.productId) {
+              item.itemQuantity = item.itemQuantity + presentItem.itemQuantity;
+              let stripObj = JSON.stringify(myObject).split('[')[1].split(']')[0];
+              localStorage.setItem('myKart', stripObj);
+              presentItem.itemQuantity = 0;
+              return;
+            }
+          });
+        }
+        else {
+          myKart = myKart + "," + JSON.stringify(this.selectedProduct);
+          localStorage.setItem('myKart', myKart);
+          this.selectedProduct.itemQuantity = 0;
+        }
+      }
+    }
+    this.utility.notyifyAll();
   }
 }
